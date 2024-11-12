306d490981c63198d968b37559f832aa39602e06
tu7 <tu7pham7@gmail.com>
2024-11-12 22:10:15 +0700

    change datatypes

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -10,7 +10,7 @@ def preprocess_categorical_data(df):
     df['Day'] = df['Day'].map({'Mon': 1, 'Tue': 2, 'Wed': 3, 'Thu': 4, 'Fri': 5, 'Sat': 6, 'Sun': 7})
     df['Venue'] = df['Venue'].map({'Home': 1, 'Away': 0})
     df['Result'] = df['Result'].map({'L': 0, 'W': 3, 'D': 1})
-    df['GF'] = df['GF'].astype(float).astype(int)
-    df['GA'] = df['GA'].astype(float).astype(int)
+    df['GF'] = df['GF'].astype(float)
+    df['GA'] = df['GA'].astype(float)
     df.drop(columns=['Notes', 'Date'], inplace=True)
     return df
